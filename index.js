517008866ea26bcafa385f458553e7e93ce9d9b8
Varun Malaviya <varunmalaviya7@gmail.com>
2023-02-05 20:19:19 -0500

    added a ternary operator to designate file path based on url. Created a PORT variable to check for port environment values and use them in case 8080 is busy or unavailable. Added a console log to show that server is up and running.

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const http = require('http')
 const path = require('path')
 const fs = require('fs')
 
+const PORT = process.env.PORT || 8080
+
 const server = http.createServer((req,res) => {
     
+    let filePath = path.join(
+                     __dirname,
+                     'public',
+                     req.url === '/' ? 'index.html' : `${req.url}.html`)
+    console.log(filePath)
 })
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
