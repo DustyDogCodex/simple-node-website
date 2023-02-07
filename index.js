a5eb3bbc72a0cc84f8f7be57c02dcd02c560a99f
Varun Malaviya <varunmalaviya7@gmail.com>
2023-02-06 20:19:19 -0500

    added nodemon as a dec dependency. Added a gitignore file to ignore mode_modules. Added comments to index.js code. Edited package.json to run dev scripts.

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const http = require('http')
 const path = require('path')
 const fs = require('fs')
 
+//accessing PORT environment values in case 8080 is unavailable
 const PORT = process.env.PORT || 8080
 
 const server = http.createServer((req,res) => {
     
+    //choosing which file will be displayed based on url
     let filePath = path.join(
                      __dirname,
                      'public',
                      req.url === '/' ? 'index.html' : `${req.url}.html`)
     console.log(filePath)
+
+    
 })
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
