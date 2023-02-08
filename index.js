479605a90d2b5e910d7deb40a8930d5d2ad1921f
Varun Malaviya <varunmalaviya7@gmail.com>
2023-02-07 20:19:19 -0500

    Finished JS code for loading and reading files based on url. Cleaned up loose characters in 404.html

--- index.js
+++ index.js
@@ -12,8 +12,28 @@ const server = http.createServer((req,res) => {
                      __dirname,
                      'public',
                      req.url === '/' ? 'index.html' : `${req.url}.html`)
-    console.log(filePath)
+    
+    //Read and load files
+    fs.readFile(filePath, (err, content) => {
 
+        if(err) {
+            //code for 404 error
+            if(err.code == "ENOENT"){
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err,content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8')
+                })
+            } else {
+                //any other server error
+                res.writeHead(500)
+                res.end(`Server Error: ${err.code}`)
+            }
+        } else {
+            //code for success
+            res.writeHead(200, { 'Content-Type' : 'text/html'})
+            res.end(content, 'utf8')
+        }
+    })
     
 })
 
